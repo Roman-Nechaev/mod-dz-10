1f3e8747fc8ea5ebaf6b77e9231fe3abbe316ea7
Roman-Nechaev <89315040+Roman-Nechaev@users.noreply.github.com>
2022-09-05 07:25:39 +0000

    Deploying to gh-pages from @ Roman-Nechaev/mod-dz-10@ef1d6a2589eeffdedf6c2df63e4d6aca5020891f 🚀

---
+++ index.1148c535.js
@@ -0,0 +1,2 @@
+!function(){var o=new URLSearchParams({_limit:5,_sort:"name"});console.log(o.toString());var c="https://jsonplaceholder.typicode.com/users?".concat(o);console.log(c)}();
+//# sourceMappingURL=index.1148c535.js.map
---
+++ index.94f0ae7f.js
@@ -0,0 +1,2 @@
+const o=new URLSearchParams({_limit:5,_sort:"name"});console.log(o.toString());const s=`https://jsonplaceholder.typicode.com/users?${o}`;console.log(s);
+//# sourceMappingURL=index.94f0ae7f.js.map
