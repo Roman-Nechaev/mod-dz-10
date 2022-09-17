fdab238bd00fa06a1fa9dc42b4eba1f4cf92c78d
Roman-Nechaev <nrchaevroman@gmail.com>
2022-09-17 20:30:38 +0300

    gogo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import API from './API/fetchCountries';
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 var _ = require('lodash');
 
 const DEBOUNCE_DELAY = 300;
@@ -11,30 +11,66 @@ const input = document.querySelector('input#search-box');
 input.addEventListener('input', _.debounce(onImput, DEBOUNCE_DELAY));
 
 function onImput(e) {
-  console.log('form');
-
   const form = e.target.value;
-  API.fetchCountries(form)
+  if (form == '') {
+    clearInput();
+  }
+
+  // const form = e.target.value;
+  API.fetchCountries(form.trim())
     .then(can => renderСountriesList(can))
     .catch(error => console.log(error));
 }
 
 function renderСountriesList(countrys) {
-  const markup = countrys
-    .map(country => {
-      return `
+  const min = 2;
+  const max = 10;
+  if (countrys.length >= max) {
+    clearInput();
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  } else if (countrys.length >= min && countrys.length <= max) {
+    clearInput();
+    console.log('от 2 до 10');
+    const markup = countrys
+      .map(country => {
+        return `
+     <div>
+     <img src=${country.flags.svg}
+      width="30"
+     height="50">
+      <p> <b>${country.name.official}</b></p> 
+            
+          </div>
+    `;
+      })
+      .join('');
+    elementСontainer.innerHTML = markup;
+  } else if (countrys.length == 1) {
+    console.log('один');
+    clearInput();
+    //////////////////////////
+    const markup = countrys
+      .map(country => {
+        return `
      <div>
      <img src=${country.flags.svg}
       width="100"
      height="100">
-            <p><b>Country</b>: ${country.name.official}</p>
+            <p><b> ${country.name.official}</b></p>
             <p><b>Capital</b>: ${country.capital}</p>
             <p><b>Population</b>: ${country.population}</p>
+             
             <p><b>Languages</b>: ${country.languages}</p>
 
           </div>
     `;
-    })
-    .join('');
-  elementСontainer.innerHTML = markup;
-}
+      })
+      .join('');
+    elementСontainer.innerHTML = markup;
+  }
+  console.log('countrys:', countrys.length);
+} //! отрисовка поисковых карточек
+
+function clearInput() {
+  elementСontainer.innerHTML = '';
+} //! очистка отрисоки при пустом инпуте
--- test.js
+++ test.js
@@ -1,45 +1,45 @@
-const searchParams = new URLSearchParams({
-  _limit: 5,
-  _sort: 'name',
-});
+// const searchParams = new URLSearchParams({
+//   _limit: 5,
+//   _sort: 'name',
+// });
 
-console.log(searchParams.toString()); // "_limit=5&_sort=name"
+// console.log(searchParams.toString()); // "_limit=5&_sort=name"
 
-const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
-console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
+// const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+// console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
 
-const fetchUsersBtn = document.querySelector('.btn');
-const userList = document.querySelector('.user-list');
+// const fetchUsersBtn = document.querySelector('.btn');
+// const userList = document.querySelector('.user-list');
 
-fetchUsersBtn.addEventListener('click', onBtnFetchUsers);
-function onBtnFetchUsers(params) {
-  fetchUsers()
-    .then(users => renderUserList(users))
-    .catch(error => console.log(error));
-}
-function fetchUsers() {
-  return fetch(
-    'https://jsonplaceholder.typicode.com/users?_limit=10&_sort=foo'
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    console.log(response);
-    return response.json();
-  });
-}
+// fetchUsersBtn.addEventListener('click', onBtnFetchUsers);
+// function onBtnFetchUsers(params) {
+//   fetchUsers()
+//     .then(users => renderUserList(users))
+//     .catch(error => console.log(error));
+// }
+// function fetchUsers() {
+//   return fetch(
+//     'https://jsonplaceholder.typicode.com/users?_limit=10&_sort=foo'
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     console.log(response);
+//     return response.json();
+//   });
+// }
 
-function renderUserList(users) {
-  const markup = users
-    .map(user => {
-      return `
-          <li>
-            <p><b>Name</b>: ${user.name}</p>
-            <p><b>Email</b>: ${user.email}</p>
-            <p><b>Company</b>: ${user.company.name}</p>
-          </li>
-      `;
-    })
-    .join('');
-  userList.innerHTML = markup;
-}
+// function renderUserList(users) {
+//   const markup = users
+//     .map(user => {
+//       return `
+//           <li>
+//             <p><b>Name</b>: ${user.name}</p>
+//             <p><b>Email</b>: ${user.email}</p>
+//             <p><b>Company</b>: ${user.company.name}</p>
+//           </li>
+//       `;
+//     })
+//     .join('');
+//   userList.innerHTML = markup;
+// }
